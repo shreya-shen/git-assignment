04b35167f13e5a7cd55cae7fa018eba27dcc64b1
shreya-shen <shreya.006@outlook.com>
2024-07-01 14:01:27 +0530

    modified java file and added a new python file : 'index.py'

--- NewProgram.java
+++ NewProgram.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 public class NewProgram{
     public static void main(String[] args) {
-        int a=5,b=2,sum=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two numbers:");
+        int a=sc.nextInt(),b=sc.nextInt(),sum=0;
         sum=a+b;
         System.out.println("Sum="+sum);
+        sc.close();
     }
 }
